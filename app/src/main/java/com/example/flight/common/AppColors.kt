13da5b81608762f11dd0dd994c5c9d7b1e6b4d66
DEJA1 <allowestq@gmail.com>
2023-02-13 23:50:19 +0100

    initial commit

---
+++ app/src/main/java/com/example/flight/common/AppColors.kt
@@ -0,0 +1,9 @@
+package com.example.flight.common
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val mBackground = Color(0xFF232424)
+    val mBackgroundSec = Color(0xFF333533)
+    val mMain = Color(0xFF6bd425)
+}
