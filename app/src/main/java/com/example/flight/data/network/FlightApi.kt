5c1a8a502e9807b9303f611510b7da8b8742c787
DEJA1 <allowestq@gmail.com>
2023-02-17 11:23:55 +0100

    fix

--- app/src/main/java/com/example/flight/data/network/FlightApi.kt
+++ app/src/main/java/com/example/flight/data/network/FlightApi.kt
@@ -1,16 +1,17 @@
 package com.example.flight.data.network
 
-import com.example.flight.common.Constants.API_KEY
-import com.example.flight.common.Resource
+import android.content.pm.ApplicationInfo
+import com.example.flight.BuildConfig
 import com.example.flight.domain.model.Location
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Query
+import java.io.FileInputStream
+import java.util.Properties
 
 interface FlightApi {
     @GET(value = "v1/flights/locations")
     suspend fun getLocations(
-        @Query("rapidapi-key") apiKey: String = API_KEY,
+        @Query("rapidapi-key") apiKey: String = BuildConfig.API_KEY,
         @Query("name") name : String,
     ) : List<Location>
 
