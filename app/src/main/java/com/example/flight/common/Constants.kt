32568e7ec5df0489b5949599e6639176af675e3d
Kamil <83213476+DEJA-1@users.noreply.github.com>
2023-02-17 11:25:55 +0100

    Update Constants.kt

--- app/src/main/java/com/example/flight/common/Constants.kt
+++ app/src/main/java/com/example/flight/common/Constants.kt
@@ -3,5 +3,5 @@ package com.example.flight.common
 object Constants {
 
     const val BASE_URL = "https://priceline-com-provider.p.rapidapi.com/"
-    const val API_KEY = "your api key"
+    const val API_KEY = "7ae121754amsh8fe93b7c7f8cd81p158f76jsna5b8561d4775"
 }
